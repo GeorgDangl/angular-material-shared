cd575bc0b8a89df58aa1edc234e6c7992704f731
Georg Dangl <georg@dangl.me>
2018-04-07 17:17:35 +0200

    Add HeaderComponent

--- src/angular-material-shared.module.ts
+++ src/angular-material-shared.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { TinyMceComponent } from './components/tiny-mce/tiny-mce.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     FooterComponent,
+    HeaderComponent,
     TinyMceComponent
+  ],
+  imports: [
+    MatToolbarModule
   ],
   exports: [
     FooterComponent,
+    HeaderComponent,
     TinyMceComponent
   ]
 })
---
+++ src/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'dangl-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  @Input() prefix: string;
+  @Input() postfix: string;
+
+}
+
