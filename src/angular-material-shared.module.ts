339eb04ca5acd1f4bde0f86b45152885d4eacb3b
Georg Dangl <georg@dangl.me>
2018-04-07 18:37:27 +0200

    Add router module for router link

--- src/angular-material-shared.module.ts
+++ src/angular-material-shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TinyMceComponent } from './components/tiny-mce/tiny-mce.component';
@@ -12,7 +13,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     TinyMceComponent
   ],
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule
   ],
   exports: [
     FooterComponent,
