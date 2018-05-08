ab5463dfa6976a80a74789b522ede254b4933148
Georg Dangl <georg@dangl.me>
2018-05-08 22:21:07 +0200

    Add support for legal notice link in footer

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'dangl-footer',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+
+  @Input() showLegalNotice = false;
+  @Input() legalNoticeText = 'Legal Notice';
+  @Input() legalNoticeLink = 'legal-notice';
+
 }
