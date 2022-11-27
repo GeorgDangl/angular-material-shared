b3a66765811d0bf19da7bbf073fa4c34fd724e69
Georg Dangl <georg@dangl.me>
2022-11-27 01:23:44 +0100

    Update to Angular v15 +semver: minor

--- src/angular-material-shared-demo/projects/angular-material-shared/src/lib/angular-material-shared.module.ts
+++ src/angular-material-shared-demo/projects/angular-material-shared/src/lib/angular-material-shared.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
