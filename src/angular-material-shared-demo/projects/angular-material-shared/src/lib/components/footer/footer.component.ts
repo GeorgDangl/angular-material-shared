b75e46c26d2b3dcaf7138d579e36a380a2058399
Borys91 <borisilchenko1991@gmail.com>
2023-08-31 10:45:58 +0300

    add privacy to footer

--- src/angular-material-shared-demo/projects/angular-material-shared/src/lib/components/footer/footer.component.ts
+++ src/angular-material-shared-demo/projects/angular-material-shared/src/lib/components/footer/footer.component.ts
@@ -1,21 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
-import { FooterOptions } from '../../models/footer-options';
+import { FooterOptions } from "../../models/footer-options";
 
 @Component({
-  selector: 'dangl-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  selector: "dangl-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent {
-
   currentYear = new Date().getFullYear();
 
   @Input() showLegalNotice = false;
-  @Input() legalNoticeText = 'Legal Notice';
-  @Input() legalNoticeLink = 'legal-notice';
+  @Input() showPrivacy = false;
+  @Input() legalNoticeText = "Legal Notice";
+  @Input() privacyText = "Privacy";
+  @Input() legalNoticeLink = "legal-notice";
+  @Input() privacyLink = "privacy";
   @Input() useLegalNoticeCallback = false;
+  @Input() usePrivaceCallback = false;
   @Input() options: FooterOptions;
   @Output() legalNoticeCallback = new EventEmitter();
-
+  @Output() privacyCallback = new EventEmitter();
 }
