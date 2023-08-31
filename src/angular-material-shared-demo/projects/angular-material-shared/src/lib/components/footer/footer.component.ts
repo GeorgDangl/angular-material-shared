82073b4496a366da6d44d90718799d17d95c9739
Borys91 <borisilchenko1991@gmail.com>
2023-08-31 12:04:57 +0300

    replace double quotes with single

--- src/angular-material-shared-demo/projects/angular-material-shared/src/lib/components/footer/footer.component.ts
+++ src/angular-material-shared-demo/projects/angular-material-shared/src/lib/components/footer/footer.component.ts
@@ -1,21 +1,21 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { FooterOptions } from "../../models/footer-options";
+import { FooterOptions } from '../../models/footer-options';
 
 @Component({
-  selector: "dangl-footer",
-  templateUrl: "./footer.component.html",
-  styleUrls: ["./footer.component.scss"],
+  selector: 'dangl-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
 
   @Input() showLegalNotice = false;
   @Input() showPrivacy = false;
-  @Input() legalNoticeText = "Legal Notice";
-  @Input() privacyText = "Privacy";
-  @Input() legalNoticeLink = "legal-notice";
-  @Input() privacyLink = "privacy";
+  @Input() legalNoticeText = 'Legal Notice';
+  @Input() privacyText = 'Privacy';
+  @Input() legalNoticeLink = 'legal-notice';
+  @Input() privacyLink = 'privacy';
   @Input() useLegalNoticeCallback = false;
   @Input() usePrivaceCallback = false;
   @Input() options: FooterOptions;
