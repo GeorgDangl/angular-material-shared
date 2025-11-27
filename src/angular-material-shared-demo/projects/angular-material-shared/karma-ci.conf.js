2837c94dd71a50e202557b3de6d1a36a62e3a6ff
ruslan-minukov <ruslan.minukov@qit.software>
2025-11-27 15:59:15 +0200

    feat: changed builder

--- src/angular-material-shared-demo/projects/angular-material-shared/karma-ci.conf.js
+++ src/angular-material-shared-demo/projects/angular-material-shared/karma-ci.conf.js
@@ -6,14 +6,13 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-junit-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage-istanbul-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
