a924a02742aafafb5a4466b0c9cc4f4b4d82bfc8
Borys91 <borisilchenko1991@gmail.com>
2026-08-26 12:09:01 +0300

    build(deps): upgrade Angular 21 to 22 with TypeScript 6
    - Angular packages: ^21.1.1 -> ^22.0.0; @angular/build and CLI to 22; ng-packagr 22
    
    - TypeScript ~5.9.3 -> ~6.0.3 (Angular 22 peer requirement)
    
    - Node engines: ^22.22.3 || ^24.15.0 || >=26.0.0
    
    - Library peers: @angular/* >=22.0.0; move @angular-devkit/schematics to devDeps
    
    - Update @types/jasmine, @types/node, jasmine-core, karma-*, puppeteer, ts-node, rxjs, tslib
    
    - tinymce ^6.8.3 -> ^7.0.0; @tinymce/tinymce-angular ^8.0.0 -> ^9.0.0
    
    - Remove codelyzer, tslint, protractor, @types/jasminewd2, zone.js
    
    - Remove lint and e2e npm scripts
    
    - tsconfig: drop baseUrl, module=es2022, types=[jasmine,node]
    
    - Fix absolute imports from src/... in main.ts
    
    - angular.json: remove -e2e project, polyfills=[]

--- src/angular-material-shared-demo/src/main.ts
+++ src/angular-material-shared-demo/src/main.ts
@@ -2,8 +2,8 @@ import { enableProdMode } from "@angular/core";
 
 import { environment } from "./environments/environment";
 import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "src/app/app.component";
-import { appConfig } from "src/app/app.config";
+import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
 
 if (environment.production) {
   enableProdMode();
