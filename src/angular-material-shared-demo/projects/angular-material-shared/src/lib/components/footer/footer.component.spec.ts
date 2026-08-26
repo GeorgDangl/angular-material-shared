cfc64a5ebb71edb4a15067049c7fb60f3b871917
Borys91 <borisilchenko1991@gmail.com>
2026-08-26 12:09:43 +0300

    test(lib): adapt specs to zoneless and strict typing
    - Replace waitForAsync with async/await
    
    - Type TinyMCE stub via interface, drop any casts, use globalThis

--- src/angular-material-shared-demo/projects/angular-material-shared/src/lib/components/footer/footer.component.spec.ts
+++ src/angular-material-shared-demo/projects/angular-material-shared/src/lib/components/footer/footer.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { RouterModule } from '@angular/router';
@@ -7,12 +7,12 @@ describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [RouterModule, FooterComponent]
     })
       .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
