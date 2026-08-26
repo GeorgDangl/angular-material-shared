3fd1fb9f508ad2437f4e9faf4cad92b043c5584d
Borys91 <borisilchenko1991@gmail.com>
2026-08-26 12:09:51 +0300

    build(karma): switch to karma-coverage
    Replace deprecated karma-coverage-istanbul-reporter with karma-coverage

--- src/angular-material-shared-demo/projects/angular-material-shared/karma-ci.conf.js
+++ src/angular-material-shared-demo/projects/angular-material-shared/karma-ci.conf.js
@@ -12,15 +12,17 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-junit-reporter'),
-      require('karma-coverage-istanbul-reporter')
+      require('karma-coverage')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' }
+      ]
     },
     reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
--- src/angular-material-shared-demo/src/karma.conf.js
+++ src/angular-material-shared-demo/src/karma.conf.js
@@ -9,15 +9,17 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter')
+      require('karma-coverage')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
